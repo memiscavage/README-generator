7a2cbdc62e9dd48da74427b9b6eb0beca7406e6c
Mariel Miscavage <memiscavage@gmail.com>
2024-03-26 11:38:08 -0500

    added comments in index.js regarding questions and prompts for user

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,14 @@
 // TODO: Include packages needed for this application
 
+
+
 // TODO: Create an array of questions for user input
+// README needs: Description, Table of Contents, Installation, Usage,
+// License, Contributing, Tests, and Questions
+//
+// Include questions on: README title, choosing a license,
+// adding a github username, email address
+//
 const questions = [];
 
 // TODO: Create a function to write README file
