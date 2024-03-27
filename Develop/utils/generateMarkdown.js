6e27e8fe93be71f2321f8c053542084703bf27b6
Mariel Miscavage <memiscavage@gmail.com>
2024-03-26 20:10:51 -0500

    updated markdown for readme sections

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,7 +14,15 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+  ## Installation
+
+  ${data.installation}
 `;
+
 }
 
 module.exports = generateMarkdown;
