4f8a7212f7ae682b76b5ffdc792a1c86c717e783
Mariel Miscavage <memiscavage@gmail.com>
2024-03-26 21:12:16 -0500

    Finished adding license links, sections, and badges to markdown file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,11 +2,15 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license==="MIT"){
+    //badges created with shields.io
     return `![Static Badge](https://img.shields.io/badge/License-MIT-brightgreen)
     `
   }
   if (license==="Apache"){
-    //create badge via 
+    return `![Static Badge](https://img.shields.io/badge/License-Apache-blue)
+    `
+  }
+  if (license==="none"){
     return ``
   }
 }
@@ -14,16 +18,27 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
+  if (license==="MIT"){
+    return `[MIT](https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt)`
+  }
   if (license==="Apache"){
     return `[Apache](http://www.apache.org/licenses/)`
   }
+  if (license==="none")
+  return ``
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  if (license==="MIT"){
+    return `A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
+  }
   if (license==="Apache"){
     return `A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
+  }
+  if (license==="none"){
+    return ``
   }
 }
 
